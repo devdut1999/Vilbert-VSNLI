06edbededcfcd1012b8830ee6234b9a84bf3c9f8
devdut1999 <65357123+devdut1999@users.noreply.github.com>
2022-06-20 15:05:48 +0530

    Update extract_image_features.sh

--- Scripts/extract_image_features.sh
+++ Scripts/extract_image_features.sh
@@ -10,6 +10,9 @@ cd ..
 
 sed -i '5,5 s/^/#/' vqa_maskrcnn_benchmark/maskrcnn_benchmark/utils/registry.py
 sed -i "s/torch._six.PY3/torch._six.PY37/" vqa_maskrcnn_benchmark/maskrcnn_benchmark/utils/imports.py
+(head -67 vqa_maskrcnn_benchmark/maskrcnn_benchmark/structures/image_list.py ; echo '        return batched_imgs' ; tail -5 vqa_maskrcnn_benchmark/maskrcnn_benchmark/structures/image_list.py)  > vqa_maskrcnn_benchmark/maskrcnn_benchmark/structures/temp.py
+rm vqa_maskrcnn_benchmark/maskrcnn_benchmark/structures/image_list.py
+mv vqa_maskrcnn_benchmark/maskrcnn_benchmark/structures/temp.py vqa_maskrcnn_benchmark/maskrcnn_benchmark/structures/image_list.py
 python3 script/extract_features.py --model_file data/detectron_model.pth --config_file data/detectron_config.yaml --image_dir "datasets/vsnli/flickr30k_images" --output_folder "datasets/vsnli/extracted_features"
 
 cd Scripts
