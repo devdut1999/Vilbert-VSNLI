0622e422b476a6f2f30cf29e9c7f71494547b589
devdut1999 <65357123+devdut1999@users.noreply.github.com>
2022-08-14 23:56:06 +0530

    Create train_pvli_transfer.sh

---
+++ Scripts/train_pvli_transfer.sh
@@ -0,0 +1,5 @@
+cd ..
+
+python3 train_tasks.py --bert_model bert-base-uncased --from_pretrained /nas/home/devadutt/Vilbert-VSNLI/Vsnli-vilbert-transfer-learning/pytorch_model_9.bin  --config_file config/bert_base_6layer_6conect.json --tasks 19 --lr_scheduler 'warmup_linear' --train_iter_gap 4 --task_specific_tokens --save_name pvli_transfer
+
+cd Scripts
