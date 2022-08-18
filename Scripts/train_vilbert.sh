9a47ff93f11c7ed96d6d7d2357cbaa843f03622d
devdut1999 <65357123+devdut1999@users.noreply.github.com>
2022-08-18 18:22:08 +0530

    Update train_vilbert.sh

--- Scripts/train_vilbert.sh
+++ Scripts/train_vilbert.sh
@@ -1,5 +1,5 @@
 cd ..
 
-python3 train_tasks.py --bert_model bert-base-uncased --from_pretrained /nas/home/devadutt/Vilbert-VSNLI/models/pretrained_model.bin  --config_file config/bert_base_6layer_6conect.json --tasks 19 --lr_scheduler 'warmup_linear' --train_iter_gap 4 --task_specific_tokens --save_name pvnli
+python3 train_tasks.py --bert_model bert-base-uncased --from_pretrained /nas/home/devadutt/Vilbert-VSNLI/models/pretrained_model.bin  --config_file config/bert_base_6layer_6conect.json --tasks 19 --lr_scheduler 'warmup_linear' --train_iter_gap 4 --task_specific_tokens --save_name pvli
 
 cd Scripts
